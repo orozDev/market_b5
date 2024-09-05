afbacf47026db219749188a5e9aaac769b08e025
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2024-09-05 18:47:35 +0600

    products | search, order, pagination

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from pprint import pprint
-
+from django.db.models import Q
+from django.core.paginator import Paginator
 from rest_framework.decorators import api_view
 
 from api.serializers import DetailProductSerializer, ListProductSerializer, ProductSerializer, \
@@ -21,8 +22,37 @@ def list_create_products(request):
         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
     products = Product.objects.all()
+
+    search = request.GET.get('search')
+
+    if search:
+        products = products.filter(
+            Q(name__icontains=search) |
+            Q(description__icontains=search) |
+            Q(content__icontains=search)
+        )
+
+    orders = ['name', 'price', 'rating', 'created_at']
+    ordering: str = request.GET.get('ordering')
+
+    if ordering and ordering.split('-')[-1] in orders:
+        products = products.order_by(ordering)
+
+    page = request.GET.get('page') or 1
+    page_size = request.GET.get('page_size') or 2
+    count = products.count()
+
+    pagin = Paginator(products, page_size)
+    products = pagin.get_page(page)
+
     serializer = ListProductSerializer(products, many=True, context={'request': request})
-    return Response(serializer.data)
+    return Response({
+        'page': page,
+        'page_size': page_size,
+        'page_count': pagin.num_pages,
+        'count': count,
+        'data': serializer.data
+    })
 
 
 @api_view(['GET', 'PATCH', 'DELETE'])
